d50978a52144ff7463badbad3b1a50bb1551e4cc
mrihm <mischa.rihm@gmail.com>
2014-11-18 22:32:41 -0600

    Commit the DateTimeAdapter code
    Commit the DateTimeAdapter class and interface which is a wrapper class
    for the DateTime object

---
+++ src/Common/DateTimeAdapter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Common
+{
+    /// <summary>
+    /// Wrapper class for the DateTime class
+    /// </summary>
+    public class DateTimeAdapter : IDateTime
+    {
+        public DateTime UtcNow 
+        {
+            get { return DateTime.UtcNow; }
+        }
+    }
+}
